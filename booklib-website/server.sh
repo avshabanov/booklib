7b107f8ce68a848a0c829c52681cd057d28994fd
Alexander Shabanov <avshabanov@gmail.com>
2014-12-24 02:25:37 -0800

    Migrating to kotlin 0.10.4, adding server script

---
+++ booklib-website/server.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+SERVICE_NAME=booklib
+PATH_TO_JAR=/usr/local/booklib/booklib-website.jar
+CONFIG_PATH=file:/usr/local/booklib/booklib.properties
+LOG_PATH=/usr/local/booklib/log
+PID_PATH_NAME=/tmp/booklib-pid
+
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup java -jar $PATH_TO_JAR --config $CONFIG_PATH 2>> $LOG_PATH.err >> $LOG_PATH.out &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR --config $CONFIG_PATH 2>> $LOG_PATH.err >> $LOG_PATH.out &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac
