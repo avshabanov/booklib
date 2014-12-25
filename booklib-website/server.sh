c4417b529e8ac478fedf589c4725d46963320b26
Alexander Shabanov <avshabanov@gmail.com>
2014-12-25 15:50:12 -0800

    Updating server script

--- booklib-website/server.sh
+++ booklib-website/server.sh
@@ -6,41 +6,40 @@ CONFIG_PATH=file:/usr/local/booklib/booklib.properties
 LOG_PATH=/usr/local/booklib/log
 PID_PATH_NAME=/tmp/booklib-pid
 
+start_server ()
+{
+    echo "Starting $SERVICE_NAME ..."
+    if [ ! -f $PID_PATH_NAME ]; then
+        nohup java -jar $PATH_TO_JAR --config $CONFIG_PATH 2>> $LOG_PATH.err >> $LOG_PATH.out &
+                    echo $! > $PID_PATH_NAME
+        echo "$SERVICE_NAME started ..."
+    else
+        echo "$SERVICE_NAME is already running ..."
+    fi
+}
+
+stop_server ()
+{
+    if [ -f $PID_PATH_NAME ]; then
+        PID=$(cat $PID_PATH_NAME);
+        echo "$SERVICE_NAME stoping ..."
+        kill $PID;
+        echo "$SERVICE_NAME stopped ..."
+        rm $PID_PATH_NAME
+    else
+        echo "$SERVICE_NAME is not running ..."
+    fi
+}
+
 case $1 in
     start)
-        echo "Starting $SERVICE_NAME ..."
-        if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR --config $CONFIG_PATH 2>> $LOG_PATH.err >> $LOG_PATH.out &
-                        echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-            echo "$SERVICE_NAME is already running ..."
-        fi
+        start_server
     ;;
     stop)
-        if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stoping ..."
-            kill $PID;
-            echo "$SERVICE_NAME stopped ..."
-            rm $PID_PATH_NAME
-        else
-            echo "$SERVICE_NAME is not running ..."
-        fi
+        stop_server
     ;;
     restart)
-        if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stopping ...";
-            kill $PID;
-            echo "$SERVICE_NAME stopped ...";
-            rm $PID_PATH_NAME
-            echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR --config $CONFIG_PATH 2>> $LOG_PATH.err >> $LOG_PATH.out &
-                        echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-            echo "$SERVICE_NAME is not running ..."
-        fi
+        stop_server
+        start_server
     ;;
 esac
