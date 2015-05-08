613e766ed4ace007cd342f1ea418359fd4be7b42
Alexander Shabanov <avshabanov@gmail.com>
2015-05-07 22:55:39 -0700

    Migrating book service to the new simple auth

--- service/book/book-server/src/main/java/com/truward/book/server/BookServerLauncher.java
+++ service/book/book-server/src/main/java/com/truward/book/server/BookServerLauncher.java
@@ -1,31 +1,16 @@
 package com.truward.book.server;
 
-import com.truward.brikar.server.auth.SimpleAuthenticatorUtil;
-import com.truward.brikar.server.auth.SimpleServiceUser;
 import com.truward.brikar.server.launcher.StandardLauncher;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-
-import javax.annotation.Nonnull;
-import java.util.Collections;
 
 /**
  * @author Alexander Shabanov
  */
-public final class BookServerLauncher extends StandardLauncher {
-
+public final class BookServerLauncher {
   public static void main(String[] args) throws Exception {
-    new BookServerLauncher().setDefaultDirPrefix("classpath:/bookService/").start(args);
-  }
-
-  //
-  // Overridden
-  //
-
-  @Override
-  protected void initServlets(@Nonnull ServletContextHandler contextHandler) {
-    super.initServlets(contextHandler);
-
-    contextHandler.setSecurityHandler(SimpleAuthenticatorUtil.newSecurityHandler(Collections.singletonList(
-        new SimpleServiceUser("testonly", "test"))));
+    new StandardLauncher()
+        .setDefaultDirPrefix("classpath:/bookService/")
+        .setSimpleSecurityEnabled(true)
+        .setAuthPropertiesPrefix("bookService.auth")
+        .start(args);
   }
 }
