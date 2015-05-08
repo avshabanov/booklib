d983a3fc71c352f6d724c5922b1cd1527fe197c5
Alexander Shabanov <avshabanov@gmail.com>
2015-05-07 22:41:59 -0700

    Migrating to brikar 1.0.8; using new simple auth approach in P13nServiceLauncher

--- service/p13n/p13n-server/src/main/java/com/truward/p13n/server/P13nServiceLauncher.java
+++ service/p13n/p13n-server/src/main/java/com/truward/p13n/server/P13nServiceLauncher.java
@@ -1,31 +1,16 @@
 package com.truward.p13n.server;
 
-import com.truward.brikar.server.auth.SimpleAuthenticatorUtil;
-import com.truward.brikar.server.auth.SimpleServiceUser;
 import com.truward.brikar.server.launcher.StandardLauncher;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-
-import javax.annotation.Nonnull;
-import java.util.Collections;
 
 /**
  * @author Alexander Shabanov
  */
-public final class P13nServiceLauncher extends StandardLauncher {
-
+public final class P13nServiceLauncher {
   public static void main(String[] args) throws Exception {
-    new P13nServiceLauncher().setDefaultDirPrefix("classpath:/p13nService/").start(args);
-  }
-
-  //
-  // Overridden
-  //
-
-  @Override
-  protected void initServlets(@Nonnull ServletContextHandler contextHandler) {
-    super.initServlets(contextHandler);
-
-    contextHandler.setSecurityHandler(SimpleAuthenticatorUtil.newSecurityHandler(Collections.singletonList(
-        new SimpleServiceUser("testonly", "test"))));
+    new StandardLauncher()
+        .setDefaultDirPrefix("classpath:/p13nService/")
+        .setSimpleSecurityEnabled(true)
+        .setAuthPropertiesPrefix("p13nService.auth")
+        .start(args);
   }
 }
